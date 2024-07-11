cd5ef570ac83cccdc56135b86f4c42763385cf2e
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-07-11 11:46:44 +0530

    upload page

--- WebDev/app/(home)/home/page.tsx
+++ WebDev/app/(home)/home/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
 import Upload from "@/components/home/upload"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Home = () => {
 
   const [isUploaded, setIsUploaded] = React.useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   return (
       <>
         <div>
-          {isUploaded? <></> : <Upload />}
+          {isUploaded? <></> : <Upload onFileUpload={setSelectedFile}/>}
         </div>
       </>
   )
--- WebDev/components/home/upload.tsx
+++ WebDev/components/home/upload.tsx
@@ -1,27 +1,98 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import { MdCloudUpload, MdDelete } from 'react-icons/md'
 import { AiFillFileImage } from 'react-icons/ai'
+import axios from 'axios';
 import '@/styles/upload.css'
 import Image from 'next/image'
-function Upload() {
-  
+
+interface UploadComponentProps {
+    onFileUpload: (file: File) => void;
+    // setUploadStatus: React.Dispatch<React.SetStateAction<string>>;
+  }
+
+const Upload: React.FC<UploadComponentProps> = ({ onFileUpload }) => {
     const [image, setImage] = useState(null)
     const [fileName, setFileName] = useState("No selected file")
+    const [selectedFileLocal, setSelectedFileLocal] = useState<File | null>(null);
+    const [csrfToken, setCsrfToken] = useState<string>('');
+    const [uploadStatus, setUploadStatus] = useState<string>('');
+
+    useEffect(() => {
+        const fetchCsrfToken = async () => {
+          console.log("fetching csrf token");
+          try {
+            const response = await axios.get('http://localhost:8000/csrf/',{
+              withCredentials: true,
+              params: {
+                username: 'your_username_here',
+              },
+            });
+            console.log("got token");
+            setCsrfToken(response.data.csrfToken);
+          } catch (error) {
+            console.error('Error fetching CSRF token:', error);
+          }
+        };
+    
+        fetchCsrfToken();
+    }, []);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const uploadedFile = event.target.files && event.target.files[0];
+        
+        if (event.target.files && event.target.files.length > 0) {
+            setFileName(event.target.files[0].name);
+            onFileUpload(event.target.files[0]);
+            setSelectedFileLocal(event.target.files[0])
+        }
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        if (!selectedFileLocal) {
+          setUploadStatus('No file selected.');
+          return;
+        }
+    
+        const formData = new FormData();
+        formData.append('file', selectedFileLocal);
+    
+        try {
+          const response = await axios.post('http://localhost:8000/upload/', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              'X-CSRFToken': csrfToken,
+              'X-APIKEY': 'api_key',
+              'X-username': 'user123'
+            },
+            withCredentials: true,
+          });
+          console.log('Response:', response.data);
+          setUploadStatus('File uploaded successfully!');
+        } catch (error: any) {
+          if (error.response) {
+            console.error('Error response data:', error.response.data);
+            console.error('Error response status:', error.response.status);
+            console.error('Error response headers:', error.response.headers);
+          } else if (error.request) {
+            console.error('Error request data:', error.request);
+          } 
+          console.error('Error message:', error.message);
+          
+          setUploadStatus('Error uploading file.');
+        }
+    };
+
     return (
       <div className='flex flex-col justify-between items-center'>
         <form
             className="flex flex-col justify-center items-center border-2 border-dashed border-primary-500 h-screens w-full cursor-pointer rounded-lg p-5 text-white"
-        // onClick={() => document.querySelector(".input-field").click()}
+            onSubmit={handleSubmit}
         >
-          <input type="file" accept='image/*' className='input-field' hidden 
-        //   onChange={({ target: {files}}) => {
-        //     files[0] && setFileName(files[0].name)
-        //     if(files){
-        //       setImage(URL.createObjectURL(files[0]))
-        //     }
-        //   }}
+          <input type="file" className='input-field' hidden
+            onChange={handleFileChange} 
            />
-  
+
           {image ?
            <Image
             src={image}
@@ -53,6 +124,9 @@ function Upload() {
             <AiFillFileImage  className="text-primary-500 w-5 h-5" />
           }
         </section>
+
+        {uploadStatus && <div className="bg-white text-black border border-gray-300 px-4 py-2 text-lg cursor-pointer">{uploadStatus}</div>}
+
   
       </div>
     )
