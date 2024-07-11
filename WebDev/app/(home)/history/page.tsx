93bc43b03612ff3eb6a137cca2da5815fee4af3c
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-07-11 08:54:22 +0530

    history page added

--- WebDev/app/(home)/history/page.tsx
+++ WebDev/app/(home)/history/page.tsx
@@ -1,54 +1,64 @@
 "use client"
 import { useCollapse } from 'react-collapsed'
 import Image from 'next/image'
-import { SlArrowDown, SlArrowUp  } from "react-icons/sl";
+import { FaCaretDown,FaCaretUp } from "react-icons/fa";
 import sampleHistory from "./sampleHistory";
 import { useState } from 'react';
 import { history } from "@/types/history";
 
 const CollapsibleList = () => {
-    const [expandedItems, setExpandedItems] = useState<{ [key: number]: boolean }>({});
   
-    const toggleExpand = (itemId:number) => {
-      setExpandedItems(prevState => ({
-        ...prevState,
-        [itemId]: !prevState[itemId],
-      }));
-    };
-  
-    return (
-      <>
-        {sampleHistory.map(item => (
-          <div key={item.id} className="w-full border border-red-200 text-white">
-            <div
-              onClick={() => toggleExpand(item.id)}
-              className='border border-red-300 flex justify-between items-center p-4 bg-gray-100 text-black select-none'
-            >
-              <span>{expandedItems[item.id] ? 'Collapse' : 'Expand'}</span> 
-              {expandedItems[item.id] ? <SlArrowUp className="w-6 h-6 text-primary-500" /> : <SlArrowDown className="w-6 h-6 text-primary-500" />}
+  return (
+    <>
+      {sampleHistory.map(item=>(
+        <CollapseData key={item.id} prop={item} />
+      ))}
+    </>
+  );
+};
+
+const CollapseData = ({prop}: {prop:history}) => {
+  const { id, desc, image} = prop;
+
+  const config = {
+    duration: 300,
+  }
+
+  const { getCollapseProps, getToggleProps, isExpanded} = useCollapse(config);
+
+  return (
+    <div className='bg-gray-200 mb-1 border rounded-md'>
+      <div 
+        className={`header flex justify-between items-center p-4 bg-gray-200 text-base1-semibold text-black-1 select-none border-dark-1 shadow-up rounded-md`}
+        {...getToggleProps()}
+      >
+        <span>{isExpanded ? 'Collapse' : 'Expand'}</span> 
+        {isExpanded ? <FaCaretUp className="w-6 h-6 text-primary-500" /> : <FaCaretDown className="w-6 h-6 text-primary-500" />}
+      </div>
+
+      <div
+        {...getCollapseProps()}
+      >
+        <div className='content border rounded-md'>
+          <div className="border border-red-300 flex flex-col lg:flex-row justify-between items-center p-2 bg-gray-100"> 
+            <div className="flex items-center justify-center w-full lg:w-1/2 p-2 mb-4 lg:mb-0">
+              <Image
+                src={image}
+                alt="logout"
+                width={50}
+                height={50}
+                className='bg-black border rounded-md'
+              />
             </div>
-            {expandedItems[item.id] && (
-              <section>
-                <div className="border border-red-300 flex flex-col lg:flex-row justify-between items-center p-4 bg-gray-100">
-                  <div className="w-full lg:w-1/2 p-2 bg-white border rounded-md mb-4 lg:mb-0">
-                    <Image
-                      src={item.image}
-                      alt="logout"
-                      width={50}
-                      height={50}
-                      className='bg-black'
-                    />
-                  </div>
-                  <div className="border border-red-300 w-full lg:w-1/2 p-2 bg-white text-primary-500 rounded-md">
-                    <span>{item.desc}</span>
-                  </div>
-                </div>
-              </section>
-            )}
-          </div>
-        ))}
-      </>
-    );
-  };
-  
-  export default CollapsibleList;
+            <div className="border border-red-300 w-full lg:w-1/2 p-2 bg-white text-black-1 rounded-md">
+              <span>{desc}</span>
+            </div>
+          </div> 
+        </div>
+      </div>
+
+    </div>
+  )
+};
+
+export default CollapsibleList;
